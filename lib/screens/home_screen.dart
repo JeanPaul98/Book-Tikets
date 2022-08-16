23f13129bd53422b311bd5551491bf5d134842aa
jean paul <89642411+JeanPaul98@users.noreply.github.com>
2022-08-16 13:16:54 +0100

    12:16 16/08/2022

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -89,6 +89,7 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
+          const Gap(15),
           TicketView(),
         ],
       ),
--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:booking/utils/app_layout.dart';
 import 'package:booking/utils/app_styles.dart';
+import 'package:booking/widgets/thick_container.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -15,37 +16,67 @@ class TicketView extends StatelessWidget {
       width: size.width,
       height: 200,
       child: Container(
-        margin: const EdgeInsets.only(left: 16),
+          margin: const EdgeInsets.only(left: 16),
           child: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              color: const Color(0xFF56799),
-              borderRadius: const BorderRadius.only(topLeft: Radius.circular(21),
-              topRight: Radius.circular(21)),
-              
-            ),
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Row(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: Color(0xFF526799),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(21),
+                      topRight: Radius.circular(21)),
+                ),
+                padding: const EdgeInsets.all(16),
+                child: Column(
                   children: [
-                    Text(
-                      "NYC",
-                      style: Styles.headLineStyle3,
-                    ),
-                    const Spacer(),
-                    Text(
-                      "London",
-                      style: Styles.headLineStyle3,
-                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "NYC",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                        Expanded(child: Container()),
+                        ThickContainer(),
+                        Expanded(
+                            child: SizedBox(
+                          height: 24,
+                          child: LayoutBuilder(
+                            builder: (BuildContext context,
+                                BoxConstraints constraints) {
+                              print(
+                                  "The width is ${constraints.constrainWidth()}");
+                              return Flex(
+                                direction: Axis.horizontal,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                    mainAxisSize: MainAxisSize.max,
+                                children:
+                                    List.generate((constraints.constrainWidth()/6).floor(), (index) => SizedBox(
+                                      width: 3, height: 1, child: DecoratedBox(decoration: BoxDecoration(
+                                        color:  Colors.white
+                                      )),
+                                    )),
+                              );
+                            },
+                          ),
+                        )),
+                        // Icon(Icons.local_airport_rounded, color: Colors.white,),
+                        Transform.rotate(angle: 1.5, child: IconTheme(data: data, child: child),),
+                        ThickContainer(),
+                        Expanded(child: Container()),
+                        Text(
+                          "London",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white),
+                        ),
+                      ],
+                    )
                   ],
-                )
-              ],
-            ),
-          )
-        ],
-      )),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
