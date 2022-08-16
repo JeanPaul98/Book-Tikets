0968a33c0f00507e955d8b687e62f2d43cde5d28
jean paul <89642411+JeanPaul98@users.noreply.github.com>
2022-08-16 12:18:28 +0100

    second commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:booking/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,21 +11,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFeeedf2),
+      backgroundColor: Styles.bgColor,
       body: ListView(
         children: [
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
+                const Gap(40),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                         Text("Bonjour"),
-                         Text("Réserver des Billets"),
+                      children: [
+                        Text(
+                          "Bonjour",
+                          style: Styles.headLineStyle3,
+                        ),
+                        const Gap(5),
+                        Text(
+                          "Réserver des Billets",
+                          style: Styles.headLineStyle1,
+                        ),
                       ],
                     ),
                     Container(
@@ -35,6 +46,43 @@ class HomeScreen extends StatelessWidget {
                             image: AssetImage("assets/images/img_1.png")),
                       ),
                     )
+                  ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xFFF4F6FD)),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_regular,
+                          color: Color(0xFFBFC205)),
+                      Text(
+                        "Search",
+                        style: Styles.headLineStyle4,
+                      ),
+                    ],
+                  ),
+                ),
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Vols à venir",
+                      style: Styles.headLineStyle2,
+                    ),
+                    InkWell(
+                        onTap: () {
+                          print("You are tapped");
+                        },
+                        child: Text(
+                          "Voir tout",
+                          style: Styles.textSyle
+                              .copyWith(color: Styles.primaryColor),
+                        )),
                   ],
                 )
               ],
