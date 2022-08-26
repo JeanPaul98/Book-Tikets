a6bbdce695531ae8fea6c646826daefce8fb766f
jean paul <89642411+JeanPaul98@users.noreply.github.com>
2022-08-26 09:29:54 +0100

    08:30 26/08/2022

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -125,7 +125,7 @@ class TicketView extends StatelessWidget {
               Container(
                 color: Styles.orangeColor,
                 child: Row(children: [
-                  SizedBox(
+                  const SizedBox(
                     height: 20,
                     width: 10,
                     child: DecoratedBox(
@@ -137,27 +137,30 @@ class TicketView extends StatelessWidget {
                           )),
                     ),
                   ),
-                  Expanded(child: LayoutBuilder(
-                    builder:
-                        (BuildContext context, BoxConstraints constraints) {
-                      return Flex(
-                        direction: Axis.horizontal,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        mainAxisSize: MainAxisSize.max,
-                        children: List.generate(
-                            (constraints.constrainWidth() / 15).floor(),
-                            (index) => SizedBox(
-                                  width: 5,
-                                  height: 1,
-                                  child: DecoratedBox(
-                                      decoration: BoxDecoration(
-                                    color: Colors.white,
+                  Expanded(child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: LayoutBuilder(
+                      builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        return Flex(
+                          direction: Axis.horizontal,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.max,
+                          children: List.generate(
+                              (constraints.constrainWidth() / 15).floor(),
+                              (index) => const SizedBox(
+                                    width: 5,
+                                    height: 1,
+                                    child: DecoratedBox(
+                                        decoration: BoxDecoration(
+                                      color: Colors.white,
+                                    )),
                                   )),
-                                )),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   )),
-                  SizedBox(
+       const            SizedBox(
                     height: 20,
                     width: 10,
                     child: DecoratedBox(
@@ -170,6 +173,49 @@ class TicketView extends StatelessWidget {
                     ),
                   ),
                 ]),
+              ),
+              //
+              Container(
+                decoration: BoxDecoration(
+                  color: Styles.orangeColor,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(21),
+                      bottomRight: Radius.circular(21)),
+                ),
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("1 MAY",
+                            style: Styles.headLineStyle3.copyWith(color: Colors.white),
+                            ),
+                            const Gap(5),
+                            Text("DATE ",
+                              style: Styles.headLineStyle3.copyWith(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text("1 MAY",
+                              style: Styles.headLineStyle3.copyWith(color: Colors.white),
+                            ),
+                            const Gap(5),
+                            Text("DATE ",
+                              style: Styles.headLineStyle3.copyWith(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               )
             ],
           )),
